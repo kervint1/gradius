f9f20d60b94bc892eb823b56f022d6f8e5fb1046
kervint1 <s1f102302124@iniad.org>
2023-07-24 19:40:23 +0900

    welcomepage fix

--- server/api/game/controller.ts
+++ server/api/game/controller.ts
@@ -1,6 +1,6 @@
 import { gameUseCase } from '$/usecase/gameUseCase';
 import { defineController } from './$relay';
-
+//controller
 export default defineController(() => ({
   post: async ({ user }) => ({ status: 201, body: await gameUseCase.createNewGame(user.id) }),
 }));
